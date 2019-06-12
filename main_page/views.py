9ef15c32d3098574ab0f97c3a8a2ef66889ce926
xedriq <xedriq@gmail.com>
2019-06-12 23:43:45 +0800

    Added templates, fixed links

--- main_page/views.py
+++ main_page/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     context = {
-        'title': 'Xedriq | Python Web Developer',
-        'test_text': 'hello from main page views',
+        'title': 'Cedrick Tabares | Python Web Developer',
     }
     template_name = 'main_page/home.html'
     return render(request, template_name, context)
