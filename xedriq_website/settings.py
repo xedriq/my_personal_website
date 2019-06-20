b1d93e47c2b544efd8d0144edd30435e18b02cac
xedriq <xedriq@gmail.com>
2019-06-21 00:19:06 +0800

    revert settings

--- xedriq_website/settings.py
+++ xedriq_website/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#=q_6w^$rry@qrdtua0ep9+tz2ka#)fhk+m(1$%1-a9^e!1nhk'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,10 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'xedriq_website_db',
-        'USERNAME': 'xedriq',
-        'PASSWORD': 'dbpassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'USERNAME': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
