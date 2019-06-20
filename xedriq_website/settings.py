febd5088d194a5379dff1593833810850c83e817
xedriq <xedriq@gmail.com>
2019-06-21 01:01:44 +0800

    fixed debug

--- xedriq_website/settings.py
+++ xedriq_website/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (os.environ['SECRET_KEY'] == 'True')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG_VALUE']
+DEBUG = (os.environ['DEBUG_VALUE'] == 'True')
 
 ALLOWED_HOSTS = []
 
